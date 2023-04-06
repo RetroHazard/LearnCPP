b61430bafee613ea61ac42e7c8294107d774b859
retrohazard <griever92@gmail.com>
2023-04-06 12:34:52 +0900

    Chapter 11 Part 3 Completed

--- Section 11/Part 3/Question 1/main.cpp
+++ Section 11/Part 3/Question 1/main.cpp
@@ -5,5 +5,19 @@
 /* Objective:
  * Print the following array to the screen using a loop
  */
+#include <iostream>
 
 constexpr int array[]{ 4, 6, 7, 3, 8, 2, 1, 9, 5 };
+
+// Begin Answer Code
+int main()
+{
+    for (int i = 0; i < static_cast<int>(std::size(array)) ; ++i) {
+        if (array[i] <= static_cast<int>(std::size(array)))
+            std::cout << array[i] << ' ';
+        else
+            break;
+    }
+//    std::cout << '\n';
+    return 0;
+}
--- Section 11/Part 3/Question 2/main.cpp
+++ Section 11/Part 3/Question 2/main.cpp
@@ -12,3 +12,47 @@
  * Then search the array for the value that the user entered and
  * print the index of that element.
  */
+#include <iostream>
+#include <iterator>
+#include <limits>
+
+constexpr int array[]{ 4, 6, 7, 3, 8, 2, 1, 9, 5 };
+
+// Begin Solution
+int getUserSelection()
+{
+    int selection{};
+    while (selection < 1 || selection > 9)
+    {
+        std::cout << "Please enter a number between 1 and 9: ";
+        std::cin >> selection;
+
+        if (std::cin.fail())
+            std::cin.clear();
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    }
+    return selection;
+}
+
+int main()
+{
+    int userSelection{getUserSelection()};
+    constexpr int arraySize{static_cast<int>(std::size(array))};
+
+    std::cout << array[userSelection];
+    for (int i = 0; i < arraySize; i++)
+        std::cout << array[i] << ' ';
+
+    std::cout << '\n';
+
+    for (int i = 0; i < arraySize ; i++) {
+        if (array[i] == userSelection)
+        {
+            std::cout << "The number " << userSelection << " has index " << i << '\n';
+            break;
+        }
+    }
+
+
+    return 0;
+}
