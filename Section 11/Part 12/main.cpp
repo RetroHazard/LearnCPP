709b4e529f00769856967f405b6da7d5429e1380
retrohazard <griever92@gmail.com>
2023-04-10 16:31:40 +0900

    Chapter 11 Part 12 Completed

--- Section 11/Part 12/main.cpp
+++ Section 11/Part 12/main.cpp
@@ -12,3 +12,28 @@
  *      std::string supports comparing strings via the comparison operators < and >.
  *          You don’t need to implement string comparison by hand.
  */
+#include <iostream>
+#include <algorithm>
+
+int main()
+{
+    std::cout << "How many names would you like to enter?: ";
+    int totalNames{};
+    std::cin >> totalNames;
+    std::string* array{new std::string[totalNames]{}};
+
+    for (int i = 0; i < totalNames; i++)
+    {
+        std::cout << "Enter name #" << i+1 << ": ";
+        std::cin >> array[i];
+    }
+
+    std::sort(&array[0],&array[totalNames]);
+
+    for (int i = 0; i < totalNames; i++)
+    {
+        std::cout << "Name #" << i+1 << ": " << array[i] << '\n';
+    }
+
+    return 0;
+}
