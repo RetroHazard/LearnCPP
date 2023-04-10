ec21ace4bd5ab879fb4b339d9de266673ec3a5a6
retrohazard <griever92@gmail.com>
2023-04-10 15:59:14 +0900

    Chapter 11 Part 12 Setup

---
+++ Section 11/Part 12/main.cpp
@@ -0,0 +1,14 @@
+//
+// Created by RetroHazard on 2023/04/10.
+//
+// https://www.learncpp.com/cpp-tutorial/dynamically-allocating-arrays/
+/* Objective:
+ * Write a program that:
+ *      Asks the user how many names they wish to enter.
+ *      Dynamically allocates a std::string array.
+ *      Asks the user to enter each name.
+ *      Calls std::sort to sort the names
+ *      Prints the sorted list of names.
+ *      std::string supports comparing strings via the comparison operators < and >.
+ *          You don’t need to implement string comparison by hand.
+ */
