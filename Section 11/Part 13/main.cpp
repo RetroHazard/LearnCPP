2e1aa2fe3dab6a1241aa02af24994daf299e0035
retrohazard <griever92@gmail.com>
2023-04-11 13:24:04 +0900

    Chapter 11 Part 13 Setup

---
+++ Section 11/Part 13/main.cpp
@@ -0,0 +1,10 @@
+//
+// Created by RetroHazard on 2023/04/11.
+//
+// https://www.learncpp.com/cpp-tutorial/for-each-loops/
+/* Objective:
+ * Declare a fixed array with the following names:
+ * Alex, Betty, Caroline, Dave, Emily, Fred, Greg, and Holly.
+ * Ask the user to enter a name.
+ * Use a for each loop to see if the name the user entered is in the array.
+ */
