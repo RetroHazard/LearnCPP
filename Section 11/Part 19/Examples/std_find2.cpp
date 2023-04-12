912777d05543c0a33e640c79ab085a238e859942
retrohazard <griever92@gmail.com>
2023-04-12 09:51:07 +0900

    Chapter 11 Part 19 Examples

---
+++ Section 11/Part 19/Examples/std_find2.cpp
@@ -0,0 +1,35 @@
+//
+// Created by RetroHazard on 2023/04/12.
+// https://www.learncpp.com/cpp-tutorial/introduction-to-standard-library-algorithms/#std_find
+#include <algorithm>
+#include <array>
+#include <iostream>
+#include <string_view>
+
+// Our function will return true if the element matches
+bool containsNut(std::string_view str)
+{
+    // std::string_view::find returns std::string_view::npos if it doesn't find
+    // the substring. Otherwise, it returns the index where the substring occurs
+    // in str.
+    return (str.find("nut") != std::string_view::npos);
+}
+
+int main()
+{
+    std::array<std::string_view, 4> arr{ "apple", "banana", "walnut", "lemon" };
+
+    // Scan our array to see if any elements contain the "nut" substring
+    auto found{ std::find_if(arr.begin(), arr.end(), containsNut) };
+
+    if (found == arr.end())
+    {
+        std::cout << "No nuts\n";
+    }
+    else
+    {
+        std::cout << "Found " << *found << '\n';
+    }
+
+    return 0;
+}
